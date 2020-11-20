df725bc9a4786e2cd5c830bd8b12e8d1cb995fb4
Ian <ianguuima@gmail.com>
2020-11-20 10:07:38 -0300

    Write first PokemonController test

--- src/test/kotlin/me/ianguuima/pokemon/controller/PokemonControllerTest.kt
+++ src/test/kotlin/me/ianguuima/pokemon/controller/PokemonControllerTest.kt
@@ -1,8 +1,56 @@
 package me.ianguuima.pokemon.controller
 
-import org.junit.jupiter.api.Assertions.*
+import me.ianguuima.pokemon.repository.PokemonRepository
+import me.ianguuima.pokemon.service.PokemonService
+import me.ianguuima.pokemon.util.PokemonCreator
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import reactor.blockhound.BlockHound
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
+@ExtendWith(SpringExtension::class)
 internal class PokemonControllerTest {
 
+    @InjectMocks
+    lateinit var pokemonController: PokemonController
+
+    @Mock
+    lateinit var pokemonService: PokemonService
+
+    private val pokemon = PokemonCreator.createValidPokemon()
+
+    companion object {
+
+        @BeforeAll
+        fun blockHoundSetup() {
+            BlockHound.install()
+        }
+
+    }
+
+    @BeforeEach
+    fun setup() {
+        BDDMockito.`when`(pokemonService.getAll())
+                .thenReturn(Flux.just(pokemon))
+    }
+
+    @Test
+    @DisplayName("find all return a flux of pokemon")
+    fun findAll_returnFluxOfAnime_WhenSuccessful() {
+        StepVerifier.create(pokemonController.getAll())
+                .expectSubscription()
+                .expectNext(pokemon)
+                .verifyComplete()
+    }
 
 }
